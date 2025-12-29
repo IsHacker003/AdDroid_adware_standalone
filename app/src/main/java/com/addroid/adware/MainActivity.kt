d5dfc7d0f498a7f685b1e1e159367f83aa88a26f
IsHacker <ishayu301@gmail.com>
2025-12-29 16:07:28 +0530

    Working on an implementation to bypass ad blockers completely

--- app/src/main/java/com/addroid/adware/MainActivity.kt
+++ app/src/main/java/com/addroid/adware/MainActivity.kt
@@ -24,9 +24,78 @@ import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.Dns
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.OkHttpClient.Builder
+import okhttp3.dnsoverhttps.DnsOverHttps
 import java.util.concurrent.atomic.AtomicBoolean
 
+
 class MainActivity : AppCompatActivity() {
+
+    val DOH_PROVIDER_URLS =
+        arrayOf(
+            "https://dns11.quad9.net/dns-query/",
+            "https://dns.google/dns-query/",
+            "https://anycast.dns.nextdns.io/dns-query/",
+            "https://130.59.31.248/dns-query/",
+            "https://dns-doh.dnsforfamily.com/dns-query/",
+            "https://94.140.14.14/dns-query/",
+            "https://1.1.1.2/dns-query/",
+            "https://pluton.plan9-dns.com/dns-query/",
+            "https://syd.adfilter.net/dns-query/",
+            "https://dnspub.restena.lu/dns-query/",
+            "https://blank.dnsforge.de/dns-query/",
+            "https://dns.mullvad.net/dns-query/",
+            "https://dns.digitale-gesellschaft.ch/dns-query/",
+            "https://dns.brahma.world/dns-query/",
+            "https://94.140.14.140/dns-query/",
+            "https://base.dns.mullvad.net/dns-query/",
+            "https://per.adfilter.net/dns-query/",
+            "https://adl.adfilter.net/dns-query/",
+            "https://family.dns.mullvad.net/dns-query/",
+            "https://dns.digitalsize.net/dns-query/",
+            "https://unicast.uncensoreddns.org/dns-query/",
+            "https://dns.circl.lu/dns-query/",
+            "https://dns12.quad9.net/dns-query/",
+            "https://all.dns.mullvad.net/dns-query/",
+            "https://dns.aa.net.uk/dns-query/",
+            "https://doh.libredns.gr/dns-query/",
+            "https://extended.dns.mullvad.net/dns-query/",
+            "https://dns.njal.la/dns-query/",
+            "https://dns9.quad9.net/dns-query/",
+            "https://149.112.112.9/dns-query/",
+            "https://freedns.controld.com/dns-query/",
+            "https://76.76.2.11/dns-query/",
+            "https://149.112.112.12/dns-query/",
+            "https://dnsforge.de/dns-query/",
+            "https://94.140.15.16/dns-query/",
+            "https://doq.dns4all.eu/dns-query/",
+            "https://ns1.fdn.fr/dns-query/",
+            "https://dns10.quad9.net/dns-query/",
+            "https://9.9.9.10/dns-query/",
+            "https://149.112.112.11/dns-query/",
+            "https://doh.ffmuc.net/dns-query/",
+            "https://odvr.nic.cz/dns-query/",
+            "https://ibksturm.synology.me/dns-query/",
+            "https://1.0.0.3/dns-query/",
+            "https://adblock.dns.mullvad.net/dns-query/",
+            "https://dns-doh-no-safe-search.dnsforfamily.com/dns-query/",
+            "https://dns1.dnscrypt.ca/dns-query/",
+            "https://cloudflare-dns.com/dns-query/",
+            "https://anycast.uncensoreddns.org/dns-query/",
+            "https://dns.quad9.net/dns-query/",
+        )
+
+    var bootstrapClient: OkHttpClient = OkHttpClient()
+    val DohNum = (0..49).random()
+    var DnsVar: Dns = DnsOverHttps.Builder().client(bootstrapClient)
+        .url(DOH_PROVIDER_URLS[DohNum].toHttpUrl())
+        .build()
+    var client: OkHttpClient = OkHttpClient.Builder().dns(DnsVar).build()
+
   private val isMobileAdsInitializeCalled = AtomicBoolean(false)
   private lateinit var binding: ActivityMainBinding
   private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
@@ -113,6 +182,10 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT,
           )
             .show()
+            try{
+                client.newCall(loadAd())
+            }
+            catch (e: Exception) {}
           applicationContext.stopService(Intent(this@MainActivity, AdsService::class.java))
           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             applicationContext.startForegroundService(Intent(this@MainActivity,
